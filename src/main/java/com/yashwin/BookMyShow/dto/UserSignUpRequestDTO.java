1d3e66254e592d4766c0675026da9a00b9a4a58f
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-11-18 16:19:30 -0500

    Added dto's

--- src/main/java/com/yashwin/BookMyShow/dto/UserSignUpRequestDTO.java
+++ src/main/java/com/yashwin/BookMyShow/dto/UserSignUpRequestDTO.java
@@ -1,4 +1,12 @@
 package com.yashwin.BookMyShow.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserSignUpRequestDTO {
+    private String name;
+    private String email;
+    private String password;
 }
--- src/main/java/com/yashwin/BookMyShow/dto/UserSignupResponseDTO.java
+++ src/main/java/com/yashwin/BookMyShow/dto/UserSignupResponseDTO.java
@@ -1,4 +1,19 @@
 package com.yashwin.BookMyShow.dto;
 
+import com.yashwin.BookMyShow.models.Ticket;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class UserSignupResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private int responseCode;
+    private String responseMessage;
+    private List<Ticket> tickets;
+
 }
