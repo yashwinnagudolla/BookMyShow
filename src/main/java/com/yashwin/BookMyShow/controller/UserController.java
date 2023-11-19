4cb94c4cb14067203df1459979352af8e87ad5fc
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-11-19 09:57:39 -0500

    Added User Controller

--- src/main/java/com/yashwin/BookMyShow/controller/UserController.java
+++ src/main/java/com/yashwin/BookMyShow/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.yashwin.BookMyShow.controller;
 
+import com.yashwin.BookMyShow.dto.UserSignUpRequestDTO;
+import com.yashwin.BookMyShow.dto.UserSignupResponseDTO;
+import com.yashwin.BookMyShow.models.User;
+import com.yashwin.BookMyShow.service.UserService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+    private UserService userService;
+
+    public UserSignupResponseDTO signUp(UserSignUpRequestDTO userSignUpRequestDTO){
+        User user;
+        UserSignupResponseDTO userSignupResponseDTO = new UserSignupResponseDTO();
+        try{
+
+
+        }catch(Exception e){
+
+        }
+
+    }
+
+
 }
