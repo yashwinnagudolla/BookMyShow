3d1c684adf615adfae24e6b9fe1b1904e5e84c1e
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-11-23 21:34:18 -0500

    Changes made in controller

--- src/main/java/com/yashwin/BookMyShow/controller/UserController.java
+++ src/main/java/com/yashwin/BookMyShow/controller/UserController.java
@@ -4,10 +4,13 @@ import com.yashwin.BookMyShow.dto.UserSignUpRequestDTO;
 import com.yashwin.BookMyShow.dto.UserSignupResponseDTO;
 import com.yashwin.BookMyShow.models.User;
 import com.yashwin.BookMyShow.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
+    @Autowired
     private UserService userService;
 
     public UserSignupResponseDTO signUp(UserSignUpRequestDTO userSignUpRequestDTO){
